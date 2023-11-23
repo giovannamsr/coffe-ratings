567b2890a76391ad4d45ba8e4fe0103f8dbfecc4
Giovanna Mendes de Sousa Rodrigues <giovanna.mendes.pro@gmail.com>
2023-11-23 17:23:29 -0300

    removed unnecessary columns

--- pre-processing.R
+++ pre-processing.R
@@ -1,11 +1,14 @@
 # Load libraries
-
 library(tidyverse)
 
 # Get the Data
-
 dados <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-07-07/coffee_ratings.csv')
 
+# Remove columns
+dados <- subset(dados, select = - c(owner, farm_name, lot_number, mill, 
+                                    ico_number, company, producer, owner_1))
+
+# Detail columns
 detail <- dados  %>%
   lapply(type_sum) %>%
   as_tibble() %>%
